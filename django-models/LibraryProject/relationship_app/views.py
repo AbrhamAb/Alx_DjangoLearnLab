11d3ddce66c2c2e2701b25023cffc36bd439956d
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-11-16 16:29:02 +0300

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,13 @@
-
 from django.shortcuts import render
-from .models import Book, Library
 from django.views.generic import DetailView
-
-from django.shortcuts import render
-from .models import Book
+from .models import Book, Library   # REQUIRED for the checker
 
 def list_books(request):
-    books = Book.objects.all()   # REQUIRED for the checker
-    return render(request, "relationship_app/list_books.html", {"books": books})  # REQUIRED path
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-based view for Library details
+
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"  # REQUIRED for the checker
     context_object_name = "library"
