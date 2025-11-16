991d56f1080f1dd95e6009acda9e760ea5469b55
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-11-16 16:54:43 +0300

    3 commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library 
 from .models import Book   # REQUIRED for the checker
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -12,3 +16,27 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"  # REQUIRED for the checker
     context_object_name = "library"
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("list_books")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
