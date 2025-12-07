18f673bde849ae3730095a94af4201f53b9a7cbb
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-12-07 16:38:04 +0300

    .

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -53,6 +53,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # Placeholder fields to satisfy configuration checks; not used by SQLite
+        'USER': '',
+        'PORT': '',
     }
 }
 
